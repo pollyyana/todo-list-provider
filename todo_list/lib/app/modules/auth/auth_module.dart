d87db073cc94af27e9b5f345492eaa6ef29a6618
pollyyana <pollyana.medeirosk@gmail.com>
2023-10-15 00:03:30 -0300

    layout tela de cadastro

--- todo_list/lib/app/modules/auth/auth_module.dart
+++ todo_list/lib/app/modules/auth/auth_module.dart
@@ -2,16 +2,23 @@ import 'package:provider/provider.dart';
 import 'package:todo_list/app/core/modules/todo_list_module.dart';
 import 'package:todo_list/app/modules/auth/login/login_controller.dart';
 import 'package:todo_list/app/modules/auth/login/login_page.dart';
+import 'package:todo_list/app/modules/auth/register/register_controller.dart';
+import 'package:todo_list/app/modules/auth/register/register_page.dart';
 
 class AuthModule extends TodoListModule {
   AuthModule()
       : super(
-          //fica as dependecias
-          bindings: [
-            ChangeNotifierProvider(create: (_)=> LoginController())
-          ],
-          routers: {
-            '/login':(context) => const LoginPage()
-          }
-        );
+            //fica as dependecias
+            bindings: [
+              ChangeNotifierProvider(
+                create: (_) => LoginController(),
+              ),
+              ChangeNotifierProvider(
+                create: (_) => RegisterController(),
+               ),
+            ],
+            routers: {
+              '/login': (context) => const LoginPage(),
+              '/register': (context) => const RegisterPage()
+            });
 }
