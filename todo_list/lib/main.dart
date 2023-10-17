616b4a0d370ad750e831fc29d959cc5a869bbc35
pollyyana <pollyana.medeirosk@gmail.com>
2023-10-17 15:35:38 -0300

    add widgetFlutterBiding

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -3,10 +3,17 @@ import 'package:todo_list/app/app_module.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async{
+// void main() async{
+//   await Firebase.initializeApp(
+//   options: DefaultFirebaseOptions.currentPlatform,
+// );
+//   runApp(const AppModule());
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AppModule());
 }
-
